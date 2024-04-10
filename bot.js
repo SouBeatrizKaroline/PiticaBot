554b1ffde478549144a3f956ebeb291a1da3c2f3
soubeatrizkaroline <soubeatrizkaroline@gmail.com>
2024-04-10 11:43:30 -0300

    Missão e Valores do CITi

--- bot.js
+++ bot.js
@@ -20,15 +20,51 @@ client.on('ready', (e) => {
   console.log(`${e.user.tag} esta online!`);
 });
 
+// ============ RESPOSTAS A MENSAGENS ============ //
+
 client.on('messageCreate', (message) => {
   if (message.author.bot) return
 
-  if (message.content === "hello") {
+  if (message.content === "!hello") {
     message.reply("world")
   }
 });
 
-client.login(process.env.TOKEN)
+
+// ============ Aréas do CITi ============ //
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return
+
+  if (message.content === "!areas") {
+    message.reply("O CITi possuí as aréas de: Pessoas, Marketing, Inteligencia de Dados e Finanças, Comercial, Design e Desenvolvimento.")
+  }
+});
+
+// ============ Missão do CITi ============ //
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return
+
+  if (message.content === "!missao") {
+    message.reply("O CITi tem como missão: Impulsionar a excelência através da inovação e pesquisa contínuas. Estamos dedicados a criar soluções  que transformem desafios em oportunidades, promovendo o crescimento sustentável do CITi e impactando positivamente a sociedade. Buscamos constantemente novos horizontes, explorando fronteiras do conhecimento e desenvolvendo soluções que impulsionem a empresa.")
+  }
+});
+
+// ============ Valores do CITi ============ //
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return
+
+  if (message.content === "!valores") {
+    message.reply("O CITi tem inumeros valores, entre eles: Colaboração, Aprendizado Contínuo, Excelência")
+  }
+});
+
+
+// ============ CONEXÃO TOKEN ============ //
+
+client.login(process.env.DISCORD_BOT_TOKEN)
   .catch(erro => {
     console.error('Falha ao fazer login:', erro);
   });
